a0258512cdccfcb2e5d17e24fea0188f311a7bf4
Saira Tabassum <sairatabassum386@gmail.com>
2023-06-10 01:08:46 +0600

    Stack Array

--- Stack/using_array_stack.cpp
+++ Stack/using_array_stack.cpp
@@ -1,8 +1,68 @@
 #include<bits/stdc++.h>>
 using namespace std;
+#define MAX 4
+
+int stack_arr[MAX],top=0;
+
+void push(int item)
+{
+
+    if(top>=MAX)
+    {
+        cout<<"Stack is full"<<endl;
+
+    }
+    else
+    {
+        stack_arr[top]=item;
+        top=top+1;
+
+    }
+
+
+}
+
+int pop()
+{
+    int item;
+    if(top==0)
+    {
+        cout<<"Stack is empty"<<endl;
+        return -1;
+    }
+
+    item=stack_arr[top-1];
+    top=top-1;
+    return item;
+
+}
+
+void print()
+{
+    for(int i=top-1; i>=0; i--)
+    {
+        cout<<stack_arr[i]<<' ';
+    }
+    cout<<endl;
+
+}
 
 int main()
 {
 
-    cout<<"i d k";
+    push(1);
+    push(2);
+    push(3);
+    print();
+    push(4);
+    push(5);
+    print();
+    pop();
+    pop();
+    push(6);
+    print();
+
+
+
+
 }
